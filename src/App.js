65ed02ef8800e074ea18bfa95b85b48b382561ce
tharunoptimus <tharunoptimus@outlook.com>
2021-04-26 11:56:39 +0530

    Revert "final commit"
    This reverts commit c2fa432ba0c0ac4aaa421cb6cbe02535f75cf879.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <p>The Desktop App to detect face reactions!</p>
-            <video 
-                id="video"
-                width="720" 
-                height="560" 
-                autoPlay 
-                muted
-            >
-
-            </video>
-        </div>
-    );
+  return (
+    <div className="App">
+      <video 
+        id="video"
+        width="720" 
+        height="560" 
+        autoPlay 
+        muted
+        ></video>
+    </div>
+  );
 }
 
 export default App;
